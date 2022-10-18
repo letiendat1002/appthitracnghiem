04e096423d2046c603c31f08c8e9f85d88204b75
Quang-Luong <quangvintage07@gmail.com>
2022-10-18 12:03:27 +0700

    hoan thanh 3 giao dien: User Management, RoomM, SubjectM

---
+++ src/RoomManagement.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class RoomManagement {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPasswordField passwordField1;
+    private JRadioButton radiobuttonOpenViewRoomManagement;
+    private JButton buttonAddViewRoomManagement;
+    private JButton buttonUpdateViewRoomManagement;
+    private JButton buttonDeleteViewRoomManagement;
+    private JButton buttonRoomResultSummaryViewRoomManagement;
+    private JButton buttonRefreshViewRoomManagement;
+    private JButton buttonBackViewRoomManagement;
+    private JTable tableViewRoomManagement;
+    private JRadioButton đóngRadioButton;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JLabel labelFindByRoomIDViewRoomManagement;
+    private JLabel labelFindBySubjectNameViewRoomManagement;
+    private JLabel labelExamIDViewRoomManagement;
+    private JLabel labelRoomTitleViewRoomManagement;
+    private JLabel labelTimeLimitViewRoomManagement;
+    private JLabel labelRoomPasswordViewRoomManagement;
+    private JLabel labelRoomStatusViewRoomManagement;
+    private JLabel labelRoomContentViewRoomManagement;
+    private JPanel radiobuttonCloseViewRoomManagement;
+}
---
+++ src/gui/SubjectManagement.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.*;
+
+public class SubjectManagement {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton buttonUpdateViewSubjectManagement;
+    private JButton buttonDeleteViewSubjectManagement;
+    private JButton buttonBackViewSubjectManagement;
+    private JButton buttonAddViewSubjectManagement;
+    private JButton buttonRefreshViewSubjectManagement;
+    private JTable tableViewSubjectManagement;
+    private JLabel labelSubjectNameViewSubjectManagement;
+    private JLabel labelFindBySubjectIDViewSubjectManagement;
+    private JLabel labelFindBySubjectNameViewSubjectManagement;
+}
