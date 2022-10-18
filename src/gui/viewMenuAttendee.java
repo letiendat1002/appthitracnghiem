7d015948097f422c8b34bbeb0466d1d716360b48
Nhut2002 <104497975+Nhut2002@users.noreply.github.com>
2022-10-18 13:10:49 +0700

    Delete src/gui directory

--- src/gui/viewMenuAttendee.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import javax.swing.*;
-
-public class viewMenuAttendee {
-    private JButton buttonGoToRoomAttendeeViewMenuAttendee;
-    private JButton buttonResultAttendeeViewMenuAttendee;
-    private JButton buttonLogoutViewMenuAttendee;
-}
