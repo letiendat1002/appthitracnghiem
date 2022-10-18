599f59b847d4e74a4f987253798a4dc200e621ee
letiendat1002 <letiendat1002@gmail.com>
2022-10-18 21:52:50 +0700

    review MenuAttendee

--- src/gui/MenuAttendee.java
+++ src/gui/MenuAttendee.java
@@ -6,4 +6,9 @@ public class MenuAttendee {
     private JButton buttonGoToRoomAttendeeViewMenuAttendee;
     private JButton buttonResultAttendeeViewMenuAttendee;
     private JButton buttonLogoutViewMenuAttendee;
+    private JPanel panelViewMenuAttendee;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
