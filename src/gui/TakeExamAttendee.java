fd6f9eae39c0d8d68bd94deec38bd1ab6f2045ce
nhut <levannhut260402@gmail.com>
2022-10-18 23:54:20 +0700

    add giao dien takeExamAttendee - nhut

--- src/gui/TakeExamAttendee.java
+++ src/gui/TakeExamAttendee.java
@@ -3,5 +3,18 @@ package gui;
 import javax.swing.*;
 
 public class TakeExamAttendee {
-    private JPanel panel1;
+    private JButton buttonStartViewTakeExamAttendee;
+    private JButton buttonSubmitViewTakeExamAttendee;
+    private JButton buttonQuitViewTakeExamAttendee;
+    private JButton buttonNextViewTakeExamAttendee;
+    private JButton buttonPrevViewTakeExamAttendee;
+    private JLabel labelRoomTitleViewTakeExamAttendee;
+    private JLabel labelRoomIDViewTakeExamAttendee;
+    private JLabel labelTimeLimitViewTakeExamAttendee;
+    private JLabel labelTotalQuestionViewTakeExamAttendee;
+    private JLabel labelDataRoomIDViewTakeExamAttendee;
+    private JLabel labelDataTimeLimitViewTakeExamAttendee;
+    private JLabel labelDataTotalQuestionViewTakeExamAttendee;
+    private JLabel labelTimeRemainingViewTakeExamAttendee;
+    private JLabel labelCountDownClockViewTakeExamAttendee;
 }
