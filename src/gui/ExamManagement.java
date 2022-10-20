3d69bc9d4ab345756f5a4970ec8e4391b601286e
letiendat1002 <letiendat1002@gmail.com>
2022-10-20 16:09:46 +0700

    Chinh sua Exam Management

--- src/gui/ExamManagement.java
+++ src/gui/ExamManagement.java
@@ -3,14 +3,10 @@ package gui;
 import javax.swing.*;
 
 public class ExamManagement {
-    private JTextField textfieldSubjectIDViewExamManagement;
-    private JTextField textfieldExamTitleViewExamManagement;
+    private JTextField textfieldSubjectNameViewExamManagement;
     private JTextField textfieldTotalQuestionViewExamManagement;
     private JTextField textfieldTotalScoreViewExamManagement;
     private JTextField textfieldScorePerQuestionViewExamManagement;
-    private JTextField textfieldEasyAmountViewExamManagement;
-    private JTextField textfieldAverageAmountViewExamManagement;
-    private JTextField textfieldHardAmountViewExamManagement;
     private JButton buttonAddViewExamManagement;
     private JButton buttonUpdateViewExamManagement;
     private JButton buttonDeleteViewExamManagement;
@@ -20,13 +16,9 @@ public class ExamManagement {
     private JTextField textfieldFindBySubjectNameViewExamManagement;
     private JTable tableViewExamManagement;
     private JLabel labelSubjectIDViewExamManagement;
-    private JLabel labelExamTitleViewExamManagement;
     private JLabel labelTotalQuestionViewExamManagement;
     private JLabel labelTotalScoreViewExamManagement;
     private JLabel labelScorePerQuestionViewExamManagement;
-    private JLabel labelEasyAmountViewExamManagement;
-    private JLabel labelAverageAmountViewExamManagement;
-    private JLabel labelHardAmountViewExamManagement;
     private JLabel labelFindByExamIDViewExamManagement;
     private JLabel labelFindBySubjectNameViewExamManagement;
     private JPanel panelViewExamManagement;
