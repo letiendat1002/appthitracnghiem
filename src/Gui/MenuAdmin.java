fbb60bcaa7dabd083bce080cbdc21e559e5b7696
Dat Tien (Dylan) Le <66414511+letiendat1002@users.noreply.github.com>
2022-10-25 14:36:00 +0700

    Cài đặt sự kiện [3] Menu (#17)(#18)
    Cài đặt sự kiện [3.1] Menu Admin + Host
    Cài đặt sự kiện [3.2] Menu Attendee

--- src/Gui/MenuAdmin.java
+++ src/Gui/MenuAdmin.java
@@ -1,16 +1,55 @@
 package Gui;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class MenuAdmin {
+public class MenuAdmin extends JFrame{
     private JButton buttonUserManagementViewMenuAdmin;
     private JButton buttonLogoutViewMenuAdmin;
-    private JButton buttonRomManagementViewMenuAdmin;
+    private JButton buttonRoomManagementViewMenuAdmin;
     private JButton buttonQuestionManagementViewMenuAdmin;
     private JButton buttonExamManagementViewMenuAdmin;
     private JPanel panelViewMenuAdmin;
 
+    public MenuAdmin(){
+        addActionEvent();
+        this.setTitle("Menu Admin");
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panelViewMenuAdmin);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    private void addActionEvent() {
+        buttonUserManagementViewMenuAdmin.addActionListener(event -> {
+           this.dispose();
+           new UserManagement();
+        });
+        buttonExamManagementViewMenuAdmin.addActionListener(event -> {
+            this.dispose();
+            new ExamManagement();
+        });
+        buttonQuestionManagementViewMenuAdmin.addActionListener(event -> {
+           this.dispose();
+           new QuestionManagement();
+        });
+        buttonRoomManagementViewMenuAdmin.addActionListener(event -> {
+            this.dispose();
+            new RoomManagement();
+        });
+        buttonLogoutViewMenuAdmin.addActionListener(event -> {
+            this.dispose();
+            new Login();
+        });
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(MenuAdmin::new);
     }
 }
--- src/Gui/MenuHost.java
+++ src/Gui/MenuHost.java
@@ -1,15 +1,52 @@
 package Gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuHost {
+public class MenuHost extends JFrame{
     private JButton buttonLogoutViewMenuHost;
     private JButton buttonRoomManagementViewMenuHost;
     private JButton buttonExamManagementViewMenuHost;
     private JButton buttonQuestionManagementViewMenuHost;
     private JPanel panelViewMenuHost;
 
+    public MenuHost(){
+        addActionEvent();
+        this.setTitle("Menu Host");
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panelViewMenuHost);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    private void addActionEvent() {
+        buttonExamManagementViewMenuHost.addActionListener(event -> {
+           this.dispose();
+           new ExamManagement();
+        });
+        buttonQuestionManagementViewMenuHost.addActionListener(event -> {
+           this.dispose();
+           new QuestionManagement();
+        });
+        buttonRoomManagementViewMenuHost.addActionListener(event -> {
+           this.dispose();
+           new RoomManagement();
+        });
+        buttonLogoutViewMenuHost.addActionListener(event -> {
+            this.dispose();
+            new Login();
+        });
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(MenuHost::new);
     }
 }
